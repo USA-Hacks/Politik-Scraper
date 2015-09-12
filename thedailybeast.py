186a0ed23b36c18c0d2804e9fc2ff334aee7957f
Pranav Sathy <sathyp@rpi.edu>
2015-09-12 04:42:40 -0400

    Removed The Slate (too unclean), have TDB as dem dataset

--- thedailybeast.py
+++ thedailybeast.py
@@ -7,31 +7,32 @@ import logging
 
 from outputwriter import OutputWriter, political_keywords
 
-url_template = u'http://www.slate.com/full_slate%s.html'
-website_name = 'http://www.slate.com/'
+url_template = u'http://www.thedailybeast.com/topics/politics.%s.html'
+website_name = '/articles'
+host_url = 'http://www.thedailybeast.com'
 num_pages = 100
 
 article_urls = []
-entity_id = 0
+entity_id = 1476
 
 def get_website_url(page_num):
-        if page_num == 0:
-                return url_template % ('')
-        else:
-                return url_template % ('.' + str(page_num))
+	return url_template % (str(page_num))
 
 
 for page in range(num_pages + 1):
-        url = get_website_url(page)
+        print 'Page: ' + str(page)
+	url = get_website_url(page)
         article = requests.get(url)
 
 	soup = BeautifulSoup(article.text, 'html.parser')
-	
 	for link in soup.find_all('a'):
 		href = link.get('href')
-		if website_name in href and 'full_slate' not in href:
-			article_urls.append(href)
+		if href != None:
+			if website_name in href:
+				article_urls.append(host_url + href)
 
+article_urls = list(set(article_urls))
+print article_urls
 out = OutputWriter()
 for article_url in article_urls:
 	try:
